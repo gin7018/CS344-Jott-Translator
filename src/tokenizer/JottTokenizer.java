ce05fd0e4c59c062a7292e54f8ab21f94fda5103
gin7018 <79063763+gin7018@users.noreply.github.com>
2022-08-30 10:22:29 -0400

    Initial project setup

---
+++ src/tokenizer/JottTokenizer.java
@@ -0,0 +1,24 @@
+package tokenizer;
+
+/**
+ * This class is responsible for tokenizing Jott code.
+ * 
+ * @author 
+ **/
+
+import java.util.ArrayList;
+
+import utils.Token;
+
+public class JottTokenizer {
+
+	/**
+     * Takes in a filename and tokenizes that file into Tokens
+     * based on the rules of the Jott Language
+     * @param filename the name of the file to tokenize; can be relative or absolute path
+     * @return an ArrayList of Jott Tokens
+     */
+    public static ArrayList<Token> tokenize(String filename){
+		return null;
+	}
+}
