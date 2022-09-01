98a1edcfe2fb41c19bcc314de744d5cc94399d21
ahb5637 <ahb5637@rit.edu>
2022-09-01 12:16:53 -0400

    Creation of program node

---
+++ src/parser/Program.java
@@ -0,0 +1,39 @@
+public class Program implements JottTree {
+    FunctionList child;
+
+
+
+
+
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        return child.convertToJott;
+    }
+
+    @Override
+    public String convertToJava() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String convertToC() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String convertToPython() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
